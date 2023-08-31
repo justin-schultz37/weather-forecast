672fe82420eeb34403d4db425fa48e2da5323104
birdslisten <justin.schultz37@gmail.com>
2023-08-31 14:16:20 -0700

    added event listeners for both the search and clear buttons

--- Assets/script.js
+++ Assets/script.js
@@ -2,3 +2,10 @@ var apiKey = 'e06830fae50fe82ed4d0b8023f6ee523';
 var inputCity = document.getElementById('input-city');
 var searchBtn = document.getElementById('search-btn');
 var clearBtn = document.getElementById('clear-btn');
+
+searchBtn.addEventListener('click', function () {
+    console.log('test if search btn working');
+})
+clearBtn.addEventListener('click', function () {
+    console.log('test if  clear btn working');
+})
