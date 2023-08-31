9d6e4f57e1192379b2f9b9f0062c9f3ede1a5273
birdslisten <justin.schultz37@gmail.com>
2023-08-31 16:01:40 -0700

    added a function to fetch the weather API and console.logs for what is being returned

--- Assets/script.js
+++ Assets/script.js
@@ -6,10 +6,22 @@ var clearBtn = document.getElementById('clear-btn');
 function getWeatherURL() {
     var apiKey = 'e06830fae50fe82ed4d0b8023f6ee523';
     var cityName = inputCity.value;
-    console.log(cityName);
-    console.log(apiKey);
+    console.log('City Name:', cityName);
+    console.log('API Key:', apiKey);
     var weatherURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=' + apiKey;
-    console.log(weatherURL);
+    console.log('Weather URL:', weatherURL);
+
+    fetch(weatherURL)
+        .then(function (response) {
+            console.log('Fetch Response:', response);
+            return response.json();
+        })
+        .then(function (data) {
+            console.log('API Data:', data);
+        })
+        .catch(function (error) {
+            console.log('Fetch Error:', error);
+        });
 }
 
 searchBtn.addEventListener('click', getWeatherURL)
