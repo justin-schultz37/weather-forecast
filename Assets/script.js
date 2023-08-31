76533a677342d0ab17a5c306814e81660fbb2f3f
birdslisten <justin.schultz37@gmail.com>
2023-08-31 14:10:36 -0700

    added HTML ids for city input and added variables that taget these element id

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,4 @@
 var apiKey = 'e06830fae50fe82ed4d0b8023f6ee523';
+var inputCity = document.getElementById('input-city');
+var searchBtn = document.getElementById('search-btn');
+var clearBtn = document.getElementById('clear-btn');
