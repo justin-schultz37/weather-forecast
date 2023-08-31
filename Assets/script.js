00816798127107127bae4d64db6566113188648d
birdslisten <justin.schultz37@gmail.com>
2023-08-31 15:45:41 -0700

    created a function that grabs city name from user input and apiKey variable

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,20 @@
-var apiKey = 'e06830fae50fe82ed4d0b8023f6ee523';
+
 var inputCity = document.getElementById('input-city');
 var searchBtn = document.getElementById('search-btn');
 var clearBtn = document.getElementById('clear-btn');
 
-searchBtn.addEventListener('click', function () {
-    console.log('test if search btn working');
-})
+function getWeatherURL() {
+    var apiKey = 'e06830fae50fe82ed4d0b8023f6ee523';
+    var cityName = inputCity.value;
+    console.log(cityName);
+    console.log(apiKey);
+    var weatherURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=' + apiKey;
+    console.log(weatherURL);
+}
+
+searchBtn.addEventListener('click', getWeatherURL)
+console.log('test if search btn working');
+
 clearBtn.addEventListener('click', function () {
     console.log('test if  clear btn working');
 })
