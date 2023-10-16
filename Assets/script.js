5d29e475dd80f67fd1f39aaa968ff80799cb8fdc
birdslisten <justin.schultz37@gmail.com>
2023-10-16 16:55:22 -0700

    fixed function that prevented user input of city and added history and clear history feature

--- Assets/script.js
+++ Assets/script.js
@@ -1,38 +1,47 @@
-
-var inputCity = document.getElementById('input-city');
-var searchBtn = document.getElementById('search-btn');
-var clearBtn = document.getElementById('clear-btn');
-var enterKey = document.querySelector('#input-city.form-control.me-2');
-
-function getWeatherURL() {
+document.addEventListener('DOMContentLoaded', function () {
+    var inputCity = document.getElementById('input-city');
+    var searchBtn = document.getElementById('search-btn');
+    var clearBtn = document.getElementById('clear-btn');
+    var historyList = document.getElementById('history-list');
     var apiKey = 'e06830fae50fe82ed4d0b8023f6ee523';
-    var cityName = inputCity.value;
-    console.log('City Name:', cityName);
-    console.log('API Key:', apiKey);
-    var weatherURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=' + apiKey;
-    console.log('Weather URL:', weatherURL);
+    var cityHistory = [];
 
-    fetch(weatherURL)
-        .then(function (response) {
-            console.log('Fetch Response:', response);
-            return response.json();
-        })
-        .then(function (data) {
-            console.log('API Data:', data);
-        })
-        .catch(function (error) {
-            console.log('Fetch Error:', error);
-        });
-}
-enterKey.addEventListener('keydown', function (e) {
-    if (e.key === 'Enter') {
-        // console.log('Enter key works');
-        getWeatherURL();
+    function getWeatherURL(cityName) {
+        var weatherURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=' + apiKey;
+        fetch(weatherURL)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log('API Data:', data);
+            })
+            .catch(function (error) {
+                console.log('Fetch Error:', error);
+            });
     }
-})
 
-searchBtn.addEventListener('click', getWeatherURL)
+    searchBtn.addEventListener('click', function () {
+        var cityName = inputCity.value;
+        getWeatherURL(cityName);
+        // Add the city name to the history
+        cityHistory.push(cityName);
+        updateHistory();
+    });
 
-clearBtn.addEventListener('click', function () {
-    console.log('test if  clear btn working');
-})
+    clearBtn.addEventListener('click', function () {
+
+        cityHistory = [];
+        updateHistory();
+    });
+
+    function updateHistory() {
+        // Clear the history list
+        historyList.innerHTML = '';
+        // Add city names to the history list
+        cityHistory.forEach(function (city) {
+            var listItem = document.createElement('li');
+            listItem.textContent = city;
+            historyList.appendChild(listItem);
+        });
+    }
+});
